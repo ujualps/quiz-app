08992205a00a95f001bc9e0a545fdd62a6a93969
ujualps <ujualps@gmail.com>
2022-04-03 22:54:30 +0530

    formatting

--- src/App.js
+++ src/App.js
@@ -81,40 +81,52 @@ function App() {
 
   const questions = [
     {
-      question: "If we want define style for an unique element, then which css selector will we use ?",
+      question:
+        "If we want define style for an unique element, then which css selector will we use ?",
       options: ["id", "text", "class", "name"],
       answer: "id",
     },
     {
-      question: "If we don't want to allow a floating div to the left side of an element, which css property will we use ?",
+      question:
+        "If we don't want to allow a floating div to the left side of an element, which css property will we use ?",
       options: ["margin", "clear", "float", "padding"],
       answer: "clear",
     },
     {
-      question: "Suppose we want to arragnge five nos. of DIVs so that DIV4 is placed above DIV1. Now, which css property will we use to control the order of stack?",
+      question:
+        "Suppose we want to arragnge five nos. of DIVs so that DIV4 is placed above DIV1. Now, which css property will we use to control the order of stack?",
       options: ["d-index", "s-index", "x-index", "z-index"],
       answer: "z-index",
     },
     {
-      question: "If we want to wrap a block of text around an image, which css property will we use ?",
+      question:
+        "If we want to wrap a block of text around an image, which css property will we use ?",
       options: ["wrap", "push", "float", "align"],
       answer: "float",
     },
     {
-      question: "If we want to show an Arrow as cursor, then which value we will use ?",
+      question:
+        "If we want to show an Arrow as cursor, then which value we will use ?",
       options: ["pointer", "default", "arrow", "arr"],
       answer: "default",
     },
     {
-      question: "If we want to use a nice looking green dotted border around an image, which css property will we use?",
-      options: ["border-color", "border-decoration", "border-style", "border-line"],
+      question:
+        "If we want to use a nice looking green dotted border around an image, which css property will we use?",
+      options: [
+        "border-color",
+        "border-decoration",
+        "border-style",
+        "border-line",
+      ],
       answer: "border-style",
     },
     {
-      question: "Which of the following properties will we use to display border around a cell without any content ?",
+      question:
+        "Which of the following properties will we use to display border around a cell without any content ?",
       options: ["empty-cell", "blank-cell", "noncontent-cell", "void-cell"],
       answer: "empty-cell",
-    }
+    },
   ];
 
   const addCorrectAnswer = () => {
